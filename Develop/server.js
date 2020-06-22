f8ec716b8ddf3cf4382a4d831fa3ecc9eb8aeaba
Aidan Q <aidanq52@gmail.com>
2020-06-22 16:34:47 -0700

    making changes to update readme and update for heroku

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes")
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
@@ -14,6 +14,10 @@ app.use(express.static("public"));
 
 app.use("/api", apiRoutes);
 app.use( htmlRoutes);
+
+app.get("/", (req, res)=>{
+    res.json(path.join(__dirname, "public/js/index.html"))
+})
 
 
 
